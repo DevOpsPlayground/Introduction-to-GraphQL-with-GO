2b95dc2249ff966a6393f1e851653d38814f22d5
Derek Wicks <derek.wicks@ecs-digital.co.uk@c02xkaddjg5j.lan>
2020-10-13 21:08:13 +0100

    Writing the tutorial

--- README.md
+++ README.md
@@ -1,1 +1,201 @@
-# Introduction-to-GraphQL-with-GO
+# Initial Set Up - Check Before You Start!
+
+This is a very challenging to implement playground where we are using a new open source technology in order to be able to deliever it. As it is all very new and one of the first playgrounds to test this out please feel free to continue the playground from your local machine at any point. 
+
+Do you have a GitHub account? If not sign up here: https://github.com/
+You may want to create a throw away GitHub accont for this playground because of integrations and having to authorise GitHub for use on our instance. 
+> Note: The instances will be shortly destroyed after the playground so any sensitive keys will be removed and not stored.
+
+Do you have GO installed?
+If you are not using the playground provided infrastructure, please do the following:-
+- Install GO
+- Create a AWS account
+- Set up the DynamoDB according to the cloudformation template at `dynamodb/dynamo.cf-template.yml`
+- Set up a user that has permissions to CRUD items on the `flights` and `passengers` table
+- Install AWS CLI on your local machine
+- Create Access Key for the user in AWS and set them up for your AWS CLI on your local machine
+
+# Stage 1: Setting up the project
+
+Create a directory for your project, and initialise it as a Go Module:
+
+`mkdir flights`
+`cd flights`
+`go mod init github.com/<GITHUB_USERNAME>/flights`
+`go get github.com/99designs/gqlgen`
+
+Create the project skeleton
+
+`go run github.com/99designs/gqlgen init`
+
+# Stage 2: Creating the schema
+
+Open `graph/schema.graphqls`
+Delete the contents of this file and replace with
+
+```go
+type Flight {
+  number: String!
+  passengers: [Passenger!]
+  capacity: Int!
+  captain: String!
+  plane: String!
+}
+
+type Passenger {
+  id: ID!
+  name: String!
+}
+
+type Query {
+  passengers: [Passenger!]
+}
+
+type Mutation {
+  createPassenger(name: String!): Passenger!
+}
+
+schema {
+  query: Query
+  mutation: Mutation
+}
+```
+
+# Stage 3: Implementation!
+
+Run the command
+`go run github.com/99designs/gqlgen generate`
+
+Don't worry about the scary looking `validation failed` and `exit status 1` output from the command
+
+Open and observe `graph/model/models_gen.go` it should contain a `Flight` and `Passenger` struct
+
+Download `datalayer/datalayer.go` from this repository and place inside your `flights` project at the location `datalayer/datalayer.go`
+
+Open `graph/schema.resolvers.go`
+Delete the content below and including `// !!! WARNING !!!`
+
+Find the func `Passengers` and replace the implementation with
+```go
+return datalayer.GetAllPassengers()
+```
+
+Run the command
+`go run ./server.go`
+
+In a web browser navigate to `http://localhost:8080`
+
+Paste the below query into the left panel of the web page
+```
+query Passengers {
+  passengers {
+    id,
+    name
+  }
+}
+```
+
+Execute the query and you should see the result
+```
+{
+  "data": {
+    "passengers": null
+  }
+}
+```
+
+This is because the DynamoDB table doesn't have any data in it yet
+
+Control-C the running command
+
+Return to your `flights` project and open `graph/schema.resolvers.go`
+
+Find the func `CreatePassenger` and replace the implementation with
+```go
+return datalayer.CreatePassenger(name)
+```
+
+Run the command
+`go run ./server.go`
+
+Open the web browser to `http://localhost:8080` again
+
+Paste the below query into the left panel of the web page
+```
+mutation CreatePassenger {
+  createPassenger(name: "Bob") {
+    id
+  }
+}
+```
+
+Execute the query and you should see the result
+```
+{
+  "data": {
+    "createPassenger": {
+      "id": "<SOME_GUID_HERE>"
+    }
+  }
+}
+```
+
+Now re-run `query Passengers` from above and you should see the id and name of the newly created passenger detailed
+
+Control-C the running command
+
+Now we've created a passenger lets put some data in the flights table
+
+To do this run the below command using the json file from `dynamodb/flight_data.json`
+`aws dynamodb batch-write-item --request-items file://flight_data.json`
+
+Return to your `flights` project and open `graph/schema.graphqls`
+Modify the `Query` type to look like this:-
+```
+type Query {
+  flights: [Flight!]
+  passengers: [Passenger!]
+}
+```
+
+Run the command
+`go run github.com/99designs/gqlgen generate`
+
+Open `graph/schema.resolvers.go`
+
+Find the func `Flights` and replace the implementation with
+```go
+return datalayer.GetAllFlights()
+```
+
+Run the command
+`go run ./server.go`
+
+Open the web browser to `http://localhost:8080` again
+
+Paste the below query into the left panel of the web page
+```
+query Flights {
+  flights {
+    number
+  }
+}
+```
+
+Execute the query and you should see the result
+```
+{
+  "data": {
+    "flights": [
+      {
+        "number": "BA-386"
+      },
+      {
+        "number": "BA-284"
+      }
+    ]
+  }
+}
+```
+
+Now lets book a passenger onto this flight
---
+++ datalayer/datalayer.go
@@ -0,0 +1,283 @@
+package datalayer
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/ecsdderekwicks/flights/graph/model"
+	"github.com/google/uuid"
+)
+
+func initialiseDb() *dynamodb.DynamoDB {
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	return dynamodb.New(sess)
+}
+
+func scanTable(svc *dynamodb.DynamoDB, tableName string) (*dynamodb.ScanOutput, error) {
+	// Build the query input parameters
+	params := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	// Make the DynamoDB Query API call
+	return svc.Scan(params)
+}
+
+func CreatePassenger(name string) (*model.Passenger, error) {
+	svc := initialiseDb()
+
+	item := model.Passenger{
+		ID:   uuid.New().String(),
+		Name: name,
+	}
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		fmt.Println("Got error marshalling new passenger item:")
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	// Create item in table passengers
+	tableName := "passengers"
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		fmt.Println("Got error calling PutItem:")
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return &item, nil
+}
+
+func DeletePassenger(passengerId string) (bool, error) {
+	svc := initialiseDb()
+
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(passengerId),
+			},
+		},
+		TableName: aws.String("passengers"),
+	}
+
+	_, err := svc.DeleteItem(input)
+	if err != nil {
+		fmt.Println("Got error calling DeleteItem")
+		fmt.Println(err.Error())
+		return false, err
+	}
+
+	return true, nil
+}
+
+// Adds "setItem" to the StringSet (SS) identified by "setAttribute" on the record with a
+// a partition key of "keyAttribute" with the value of "key" in the Dynamo table "table".
+func addToSet(db *dynamodb.DynamoDB, table, keyAttribute, key, setAttribute, setItem string) error {
+	_, err := db.UpdateItem(&dynamodb.UpdateItemInput{
+		ExpressionAttributeNames: map[string]*string{
+			"#0": &setAttribute,
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":0": {SS: []*string{&setItem}},
+		},
+		Key: map[string]*dynamodb.AttributeValue{
+			keyAttribute: {S: &key},
+		},
+		TableName:        &table,
+		UpdateExpression: aws.String("ADD #0 :0"),
+	})
+	return err
+}
+
+// Deletes "setItem" from the StringSet (SS) identified by "setAttribute" on the record with a
+// a partition key of "keyAttribute" with the value of "key" in the Dynamo table "table".
+func deleteFromSet(db *dynamodb.DynamoDB, table, keyAttribute, key, setAttribute, setItem string) error {
+	_, err := db.UpdateItem(&dynamodb.UpdateItemInput{
+		ExpressionAttributeNames: map[string]*string{
+			"#0": &setAttribute,
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":0": {SS: []*string{&setItem}},
+		},
+		Key: map[string]*dynamodb.AttributeValue{
+			keyAttribute: {S: &key},
+		},
+		TableName:        &table,
+		UpdateExpression: aws.String("DELETE #0 :0"),
+	})
+	return err
+}
+
+func BookFlight(flightNumber string, passengerId string) (bool, error) {
+	svc := initialiseDb()
+
+	err := addToSet(svc, "flights", "number", flightNumber, "passengers", passengerId)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return false, err
+	}
+
+	return true, nil
+}
+
+func CancelBooking(flightNumber string, passengerId string) (bool, error) {
+	svc := initialiseDb()
+
+	err := deleteFromSet(svc, "flights", "number", flightNumber, "passengers", passengerId)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return false, err
+	}
+
+	return true, nil
+}
+
+type DynamoFlight struct {
+	Number     string
+	Passengers []string
+	Capacity   int
+	Captain    string
+	Plane      string
+}
+
+func GetAllFlights() ([]*model.Flight, error) {
+	svc := initialiseDb()
+
+	result, err := scanTable(svc, "flights")
+
+	if err != nil {
+		fmt.Println("Query API call failed:")
+		fmt.Println((err.Error()))
+		return nil, err
+	}
+
+	var flights []*model.Flight
+
+	for _, dynamoItem := range result.Items {
+		item := DynamoFlight{}
+
+		err = dynamodbattribute.UnmarshalMap(dynamoItem, &item)
+
+		if err != nil {
+			fmt.Println("Got error unmarshalling:")
+			fmt.Println(err.Error())
+			return nil, err
+		}
+
+		flight, err := convertDynamoFlightToFlight(item)
+
+		if err != nil {
+			return nil, err
+		}
+
+		flights = append(flights, flight)
+	}
+
+	return flights, nil
+}
+
+func convertDynamoFlightToFlight(dynamoFlight DynamoFlight) (*model.Flight, error) {
+	flight := model.Flight{
+		Number:   dynamoFlight.Number,
+		Capacity: dynamoFlight.Capacity,
+		Captain:  dynamoFlight.Captain,
+		Plane:    dynamoFlight.Plane,
+	}
+
+	for _, passengerId := range dynamoFlight.Passengers {
+		passenger, err := GetPassenger(passengerId)
+
+		if err != nil {
+			fmt.Println("Query API call failed:")
+			fmt.Println((err.Error()))
+			return nil, err
+		}
+
+		flight.Passengers = append(flight.Passengers, passenger)
+	}
+
+	return &flight, nil
+}
+
+func GetPassenger(passengerId string) (*model.Passenger, error) {
+	svc := initialiseDb()
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(passengerId),
+			},
+		},
+		TableName: aws.String("passengers"),
+	}
+
+	dynamoItem, err := svc.GetItem(input)
+
+	if err != nil {
+		fmt.Println("Query API call failed:")
+		fmt.Println((err.Error()))
+		return nil, err
+	}
+
+	item := model.Passenger{}
+
+	err = dynamodbattribute.UnmarshalMap(dynamoItem.Item, &item)
+
+	if err != nil {
+		fmt.Println("Got error unmarshalling:")
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return &item, nil
+}
+
+func GetAllPassengers() ([]*model.Passenger, error) {
+	svc := initialiseDb()
+
+	result, err := scanTable(svc, "passengers")
+
+	if err != nil {
+		fmt.Println("Query API call failed:")
+		fmt.Println((err.Error()))
+		return nil, err
+	}
+
+	var passengers []*model.Passenger
+
+	for _, dynamoItem := range result.Items {
+		item := model.Passenger{}
+
+		err = dynamodbattribute.UnmarshalMap(dynamoItem, &item)
+
+		if err != nil {
+			fmt.Println("Got error unmarshalling:")
+			fmt.Println(err.Error())
+			return nil, err
+		}
+
+		passengers = append(passengers, &item)
+	}
+
+	return passengers, nil
+}
